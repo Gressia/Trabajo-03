185a57e20cce410ea31b16a3890d8f7ce1390977
Gressia <57844074+gressia@users.noreply.github.com>
2019-11-17 02:28:05 -0500

    Ejercicios 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40

---
+++ Alicia Vilchez Orlandini.py
@@ -0,0 +1,540 @@
+#Ejercicio_21
+#Boleta_de_venta
+#DECLARACION
+libro_auditoria_tributaria=0.0
+libro_manual_contador=0.0
+compendio_laboral=0.0
+Total=0.0
+
+#INPUT
+libro_de_auditoria_tributaria=250.00
+libro_manual_del_contador=250.00
+compendio_laboral=300.00
+
+#PROCESSING
+Total=(libro_de_auditoria_tributaria+libro_manual_del_contador+compendio_laboral)
+
+#OUTPUT
+print("########################")
+print("     Boleta de venta    ")
+print("########################")
+print("libro de auditoria tributaria:",libro_de_auditoria_tributaria)
+print("libro manual del contador:",libro_manual_del_contador)
+print("compendio laboral:",compendio_laboral)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_22
+#Factura
+#DECLARACION
+Servicio_de_soporte_tecnico=0.0
+Reparacion_sistema_operativo_smartphone=0.0
+venta_mouse=0.0
+venta_teclado=0.0
+venta_pantalla=0.0
+Memoria_USB=0.0
+Total=0.0
+
+#INPUT
+Servicio_de_soporte_tecnico=80.00
+Reparacion_sistema_operativo_smartphone=35.00
+venta_mouse=10.00
+venta_teclado=25.00
+venta_pantalla=60.00
+Memoria_USB=20.00
+
+#PROCESSING
+Total=(Servicio_de_soporte_tecnico+Reparacion_sistema_operativo_smartphone+venta_mouse+venta_teclado+venta_pantalla+Memoria_USB)
+
+#OUTPUT
+print("###########################")
+print("          Factura          ")
+print("###########################")
+print("Servicio de soporte tecnico:",Servicio_de_soporte_tecnico)
+print("Reparacion sistema operativo smartphone:",Reparacion_sistema_operativo_smartphone)
+print("venta mouse:",venta_mouse)
+print("venta teclado:",venta_teclado)
+print("venta pantalla:",venta_pantalla)
+print("Memoria USB:",Memoria_USB)
+print("Total:",Total)
+print("###########################")
+
+#Ejercicio_23
+#Recibo de agua
+#DECLARACION
+Servicio_agua=0.0
+Servicio_desague=0.0
+cargo_fijo=0.0
+intereses_moras=0.0
+IGV=0.0
+Redondeo=0.0
+Total=0.0
+
+#INPUT
+Servicio_agua=14.04
+Servicio_desague=6.22
+cargo_fijo=2.32
+intereses_moras=0.04
+IGV=4.06
+Redondeo=0.02
+
+#PROCESSING
+Total=(Servicio_agua+Servicio_desague+cargo_fijo+intereses_moras+IGV+Redondeo)
+
+#OUTPUT
+print("########################")
+print("      Recibo de agua    ")
+print("########################")
+print("Servicio de agua:",Servicio_agua)
+print("Servicio de desague:",Servicio_desague)
+print("cargo fijo:",cargo_fijo)
+print("interes y mora:",intereses_moras)
+print("IGV:",IGV)
+print("Redondeo:",Redondeo)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_24
+#Voucher del Banco de la Nacion
+#DECLARACION
+Saldo_anterior=0.0
+Retiro_efectivo=0.0
+Saldo_total=0.0
+
+#INPUT
+Saldo_anterior=12630.00
+Retiro_efectivo=350.00
+
+#PROCESSING
+Saldo_total=(Saldo_anterior-Retiro_efectivo)
+
+#OUTPUT
+print("######################################")
+print("    Voucher del Banco de la Nacion    ")
+print("######################################")
+print("Saldo anterior:",Saldo_anterior)
+print("Retiro en efectivo:",Retiro_efectivo)
+print("Saldo total:",Saldo_total)
+print("#######################################")
+
+#Ejercicio_25
+#Recibo de luz
+#DECLARACION
+Cargo_fijo=0.0
+cargo_reposicion_mantenimiento=0.0
+energia_activas=0.0
+alumbrado_publico=0.0
+interes_compensatorio=0.0
+Sub_total=0.0
+Imp_general_ventas=0.0
+saldo_redondeo=0.0
+aporte_ley=0.0
+Total=0.0
+
+#INPUT
+Cargo_fijo=3.12
+cargo_reposicion_mantenimiento=1.12
+energia_activas=3.00
+alumbrado_publico=0.42
+interes_compensatorio=0.01
+Sub_total=7.67
+Imp_general_ventas=1.38
+saldo_redondeo=0.01
+aporte_ley=0.06
+
+#PROCESSING
+Total=(Cargo_fijo+cargo_reposicion_mantenimiento+energia_activas+alumbrado_publico+interes_compensatorio+Sub_total+Imp_general_ventas+saldo_redondeo+aporte_ley)
+
+#OUTPUT
+print("########################")
+print("     Recibo de luz      ")
+print("########################")
+print("Cargo fijo:",Cargo_fijo)
+print("cargo por reposicion y mantenimiento:",cargo_reposicion_mantenimiento)
+print("energia activas:",energia_activas)
+print("alumbrado publico:",alumbrado_publico)
+print("interes_compensatorio:",interes_compensatorio)
+print("Subtotal:",Sub_total)
+print("Importe deneral a las ventas:",Imp_general_ventas)
+print("saldo por redonde:",saldo_redondeo)
+print("aporte ley:",aporte_ley)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_26
+#Boleta de venta Elicar SAC
+#DECLARACION
+olla_fierro_grande=0.0
+olla_fierro_mediana=0.0
+Total=0.0
+
+#INPUT
+olla_fierro_grande=150.00
+olla_fierro_mediana=100.00
+
+#PROCESSING
+Total=(olla_fierro_grande+olla_fierro_mediana)
+
+#OUTPUT
+print("###################################")
+print("     Boleta de venta Elicar SAC    ")
+print("###################################")
+print("olla de fierro grande:",olla_fierro_grande)
+print("olla de fierro mediana:",olla_fierro_mediana)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_27
+#Promedio de notas
+#DECLARACION
+nota1,nota2,nota3=0.0,0.0,0.0
+promedio=0.0
+codigo=" "
+
+#INPUT
+nota1=18
+nota2=13
+nota3=15
+
+#PROCESSING
+promedio=(nota1+nota2+nota3)/3
+
+#OUTPUT
+print("promedio:",promedio)
+
+#Ejercicio_28
+#Factura Multicines
+#DECLARACION
+costo_pelicula_uno=0.0
+costo_pelicula_dos=0.0
+Total=0.0
+
+#INPUT
+costo_pelicula_uno=7.00
+costo_pelicula_dos=7.00
+
+#PROCESSING
+Total=(costo_pelicula_uno+costo_pelicula_dos)
+
+#OUTPUT
+print("Total:",Total)
+
+#Ejercicio_29
+#Empresa de Transporte Chiclayo
+#DECLARACION
+precio_pasaje_ida_piura=0.0
+precio_pasaje_regreso_chiclayo=0.0
+Total=0.0
+
+#INPUT
+precio_pasaje_ida_piura=22.00
+precio_pasaje_regreso_chiclayo=24.00
+
+#PROCESSING
+Total=(precio_pasaje_ida_piura+precio_pasaje_regreso_chiclayo)
+
+#OUTPUT
+print("Total:",Total)
+
+#Ejercicio_30
+#Corporacion Cultural Boleta
+#DECLARACION
+libro_matematicas=0.0
+libro_Biologia=0.0
+libro_Ciencias=0.0
+Total=0.0
+
+#INPUT
+libro_matematicas=125.00
+libro_Biologia=164.00
+libro_Ciencias=78.00
+
+#PROCESSING
+Total=(libro_matematicas+libro_Biologia+libro_Ciencias)
+
+#OUTPUT
+print("#################################")
+print("   Corporacion Cultural Boleta  ")
+print("#################################")
+print("libro de matematicas:",libro_matematicas)
+print("libro de Biologia:",libro_Biologia)
+print("libro de Ciencias:",libro_Ciencias)
+print("Total:",Total)
+print("#################################")
+
+#Ejercicio_31
+#Boleto de viaje Turismo Barranca
+#DECLARACION
+pasaje1=0.0
+pasaje2=0.0
+pasaje3=0.0
+Total=0.0
+
+#INPUT
+pasaje1=12.00
+pasaje2=12.00
+pasaje3=12.00
+
+#PROCESSING
+Total=(pasaje1+pasaje2+pasaje3)
+
+#OUTPUT
+print("####################################")
+print("  Boleto de viaje Turismo Barranca  ")
+print("########################")
+print("Total:",Total)
+print("####################################")
+
+#Ejercicio_32
+#Recibo de Ingresos FAcFYM
+#DECLARACION
+fut1=0.0
+fut2=0.0
+Total=0.0
+
+#INPUT
+fut1=2.00
+fut2=2.00
+
+#PROCESSING
+Total=(fut1+fut2)
+
+#OUTPUT
+print("##############################")
+print("  Recibo de Ingresos FAcFYM   ")
+print("########################")
+print("fut1:",fut2)
+print("fut2:",fut1)
+print("Total:",Total)
+print("##############################")
+
+#Ejercicio_33
+#Dino Boleta
+#DECLARACION
+barillas2=0.0
+bolsas_cemento36=0.0
+porcelanato_cajas8=0.0
+Total=0.0
+
+#INPUT
+barillas2=65.00
+bolsas_cemento36=432.00
+porcelanato_cajas8=700
+
+#PROCESSING
+Total=(barillas2+bolsas_cemento36+porcelanato_cajas8)
+
+#OUTPUT
+print("########################")
+print("    Dino Boleta       ")
+print("########################")
+print(" dos barillas:",barillas2)
+print("bolsas de cemento:",bolsas_cemento36)
+print("cajas de porcelanato:",porcelanato_cajas8)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_34
+#Boleta de Matricula ICPNA
+#DECLARACION
+matricula=0.0
+modulos3=0.0
+Total=0.0
+
+#INPUT
+matricula=100.00
+modulos3=150.00
+
+#PROCESSING
+Total=(matricula+modulos3)
+
+#OUTPUT
+print("##############################")
+print("  Boleta de Matricula ICPNA   ")
+print("########################")
+print("matricula:",matricula)
+print("tres modulos:",modulos3)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_35
+#Boleta de ropa
+#DECLARACION
+casacas3=0.0
+pantalon=0.0
+blusas2=0.0
+poleras3=0.0
+Total=0.0
+
+#INPUT
+casacas3=115.00
+pantalon=120.00
+poleras3=80.00
+
+#PROCESSING
+Total=(casacas3*3+pantalon+poleras3*3)
+
+#OUTPUT
+print("########################")
+print("     Boleta de ropa     ")
+print("########################")
+print("tres casacas:",casacas3)
+print("pantalon:",pantalon)
+print("tres poleras:",poleras3)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_36
+#Boleta de utiles escolares
+#DECLARACION
+cuadernos_cuadriculados6=0.0
+cuadernos_rayado3=0.0
+tijeras2=0.0
+goma=0.0
+vinifan2=0.0
+colores=0.0
+plumones3=0.0
+folders3=0.0
+Total=0.0
+
+#INPUT
+cuadernos_cuadriculados6=7.50
+cuadernos_rayado3=7.50
+tijeras2=3.00
+goma=1.50
+vinifan2=4.50
+colores=36
+plumones3=2.50
+folders3=4.00
+
+#PROCESSING
+Total=(cuadernos_cuadriculados6*6+cuadernos_rayado3*3+tijeras2*2+goma+vinifan2*2+colores+plumones3*3+folders3*3)
+
+#OUTPUT
+print("##################################")
+print("     Boleta de utiles escolares   ")
+print("##################################")
+print("seis cuadernos cuadriculados:",cuadernos_cuadriculados6)
+print("tres cuadernos rayados:",cuadernos_rayado3)
+print("dos tijeras:",tijeras2)
+print("goma:",goma)
+print("dos vinifan:",vinifan2)
+print("colores:",colores)
+print("tres plumones:",plumones3)
+print("tres folders:",folders3)
+print("Total:",Total)
+print("##################################")
+
+#Ejercicio_37
+#Boleta de venta
+#DECLARACION
+paquetes5_gaseosas=0.0
+cajas2_atunes=0.0
+cajas2_papaslays=0.0
+caja_galleta_ore=0.0
+bolsas2_chisitos=0.0
+saco2_arroz=0.0
+Total=0.0
+
+#INPUT
+paquetes5_gaseosas=225.00
+cajas2_atunes=144.00
+cajas2_papaslays=120.00
+caja_galleta_ore=25.00
+bolsas2_chisitos=12.00
+saco2_arroz=240
+
+#PROCESSING
+Total=(paquetes5_gaseosas+cajas2_atunes+cajas2_papaslays+caja_galleta_ore+bolsas2_chisitos+saco2_arroz)
+
+#OUTPUT
+print("########################")
+print("     Boleta de venta    ")
+print("########################")
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_38
+#Informacion de gastos
+#DECLARACION
+sueldo=0.0
+pago_luz=0.0
+pago_agua=0.0
+pago_internet=0.0
+pago_gas=0.0
+pago_cuentas=0.0
+Total=0.0
+
+#INPUT
+sueldo=1800.00
+pago_luz=100.00
+pago_agua=29.30
+pago_internet=99.90
+pago_gas=25.00
+pago_cuentas=150.00
+
+#PROCESSING
+Total=(pago_luz+pago_agua+pago_internet+pago_gas+pago_cuentas)
+
+#OUTPUT
+print("###########################")
+print("    Informacion de gatos   ")
+print("###########################")
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_39
+#Boleta_de_venta
+#DECLARACION
+libro_auditoria_tributaria=0.0
+libro_manual_contador=0.0
+compendio_laboral=0.0
+Total=0.0
+
+#INPUT
+libro_de_auditoria_tributaria=250.00
+libro_manual_del_contador=250.00
+compendio_laboral=300.00
+
+#PROCESSING
+Total=(libro_de_auditoria_tributaria+libro_manual_del_contador+compendio_laboral)
+
+#OUTPUT
+print("########################")
+print("     Boleta de venta    ")
+print("########################")
+print("libro de auditoria tributaria:",libro_de_auditoria_tributaria)
+print("libro manual del contador:",libro_manual_del_contador)
+print("compendio laboral:",compendio_laboral)
+print("Total:",Total)
+print("########################")
+
+#Ejercicio_40
+#Boleta de Compra
+#DECLARACION
+televisor=0.0
+refrigeladora=0.0
+horno_microondas=0.0
+plancha=0.0
+cocina=0.0
+muebles=0.0
+Total=0.0
+
+#INPUT
+televisor=2000.00
+refrigeladora=1200.00
+horno_microondas=450.00
+plancha=250.00
+cocina=1500.00
+muebles=2800.00
+
+#PROCESSING
+Total=(televisor+refrigeladora+horno_microondas+plancha+cocina+muebles)
+
+#OUTPUT
+print("########################")
+print("     Boleta de compra   ")
+print("########################")
+print("Total:",Total)
+print("########################")
+
